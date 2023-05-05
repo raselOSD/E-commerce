0e293a8106ffdeca0aaa65c861c859138d8d5fd1
raselOSD <raselhaider2019@gmail.com>
2023-05-05 16:00:49 +0600

    Project upload

---
+++ app/src/main/java/com/example/firebase/FirebaseFirestore.java
@@ -0,0 +1,12 @@
+package com.example.firebase;
+
+public class FirebaseFirestore {
+    public Object collection;
+
+    public static FirebaseFirestore getInstance() {
+        return null;
+    }
+
+    public void collection(String user) {
+    }
+}
